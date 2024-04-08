c9a23aa33955a543a2640187a89b3f0c1ebb83ad
AmoghParulekar2003 <amoghmparulekar@gmail.com>
2024-04-08 23:35:53 +0530

    number guessing game updated

--- numberguess.cpp
+++ numberguess.cpp
@@ -7,12 +7,17 @@ int main() {
     int tries = 0;
 
     srand(time(NULL));
-    num = (rand() % 10) + 1;
+    num = (rand() % 100) + 1;
 
     std::cout<<"**********NUMBER GUESSING GAME**********\n";
     do {
-        std::cout<<"Enter a guess between 1 and 10: ";
+        std::cout<<"Enter a guess between 1 and 100: ";
         std::cin>>guess;
+        if(guess > num) {
+            std::cout<<"TOO HIGH\n";
+        } else if(guess < num) {
+            std::cout<<"TOO LOW\n";
+        }
         tries++;
     } while(guess != num);
 
